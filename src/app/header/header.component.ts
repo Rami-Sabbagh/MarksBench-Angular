ff2b771ea6703cf1eda4039103ed4d87bd3fa160
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-07-09 21:22:18 +0300

    Add styles to the chosen data format in the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { mdiGithub } from '@mdi/js';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+	/**
+	 * The available data formats that the application can handle.
+	 */
+	public dataFormats: string[] = ['JSON (arrays)', 'JSON (objects)', 'CSV'];
 
-  constructor() { }
+	/**
+	 * The selected data format for the PDF documents.
+	 */
+	public selectedDataFormat: string = 'CSV';
 
-  ngOnInit(): void {
-  }
+	/* -- Icons that are bound to the template -- */
+	public mdiGithub: string = mdiGithub;
 
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	/**
+	 * Changes the selected data format type
+	 * to the user's choice.
+	 *
+	 * @param dataFormat The new data format that the user selects.
+	 */
+	changeSelectedFormat(dataFormat: string): void {
+		this.selectedDataFormat = dataFormat;
+	}
 }
