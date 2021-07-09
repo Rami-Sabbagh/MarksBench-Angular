4fc415970cb75de34bcb8b31dde95db479ffc56e
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-07-09 17:51:37 +0300

    Create the initial application's interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DocumentsListComponent } from './documents-list/documents-list.component';
+import { DocumentRowComponent } from './documents-list/document-row/document-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DocumentsListComponent,
+    DocumentRowComponent
   ],
   imports: [
     BrowserModule
