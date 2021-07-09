c557dc55e8968f1c398da73de1e2ebedbeaaf5a5
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-07-09 17:51:08 +0300

    Create document related components

---
+++ src/app/documents-list/document-row/document-row.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-row',
+  templateUrl: './document-row.component.html',
+  styleUrls: ['./document-row.component.scss']
+})
+export class DocumentRowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
